ac350994d9a140e93adc09ecdcf89e039f101ee4
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2022-12-07 09:41:05 +0630

    straw hat

---
+++ main.js
@@ -0,0 +1,231 @@
+let strawHatCrew = [
+    {
+        id: 1,
+        name: "Monkey D. Luffy",
+        profession: "Captain",
+        capabilities: [
+            "Gomu Gomu no Mi (Awakened)",
+            "All three forms of Haki (Advanced)",
+            "Poison Resistance",
+            "Voice of All Things",
+            "Hand-to-Hand Combatant"
+        ],
+        ethithet: "Straw Hat",
+        bounty: 3000000000,
+        address: "East Blue (Goa Kingdom)",
+        age: 19,
+        gender: "Male",
+        Height: "174cm",
+        Blood: "F",
+        Birthday: "May 5th"
+    },
+    {
+        id: 2,
+        name: "Roronoa Zoro",
+        profession: "Swordsman and unofficial first mate",
+        capabilities: [
+            {
+                Swordsmanship: [
+                "Three Sword Style",
+                "Two Sword Style",
+                "One Sword Style",
+                "Nine Sword Style",
+                "No Sword Style",
+                "Foxfire Style"
+                ]
+            },
+            "Kenbunshoku Haki",
+            "Busoshoku Haki",
+            "Advanced Haoshoku Haki"
+        ],
+        ethithet: "Pirate Hunter",
+        bounty: 1111000000,
+        address: "East Blue",
+        age: 21,
+        gender: "Male",
+        Height: "181cm",
+        Blood: "XF",
+        Birthday: "November 11th"
+    },
+    {
+        id: 3,
+        name: "Nami",
+        profession: "Navigator",
+        capabilities: [
+            "Cartographer",
+            {
+                ArtOfWeather: [
+                    "Sorcery Clima-Tact",
+                    "Zeus"
+                ],
+            },
+            "Bo Staff Fighter"
+        ],
+        ethithet: "Cat Burglar",
+        bounty: 366000000,
+        address: "East Blue (Conomi Islands)",
+        age: 20,
+        gender: "Female",
+        Height: "170cm",
+        Blood: "X",
+        Birthday: "July 3rd"
+    },
+    {
+        id: 4,
+        name: "Usopp",
+        profession: "Sniper",
+        capabilities: [
+            {
+                WeaponArsenal: [
+                    "Kuro Kabuto",
+                    "Pop Green Ammunition"
+                ]
+            },
+            "Weapon Inventor",
+            "Kenbunshoku Haki"
+        ],
+        ethithet: ["Sogeking", "God"],
+        bounty: 500000000,
+        address: "East Blue (Gecko Islands)",
+        age: 19,
+        gender: "Male",
+        Height: "176cm",
+        Blood: "S",
+        Birthday: "April 1st"
+    },
+    {
+        id: 5,
+        name: "Sanji",
+        profession: "Chef",
+        capabilities: [
+            {
+                BlackLegStyle: [
+                    "Diable Jambe",
+                    "Ifrit Jambe"
+                ]
+            },
+            "Kenbunshoku Haki",
+            "Busoshoku Haki",
+            "Modified Human",
+            "Attack Cuisine"
+        ],
+        ethithet: "Black Leg",
+        bounty:  1032000000,
+        address: "North Blue",
+        age: 21,
+        gender: "Male",
+        Height: "180cm",
+        Blood: "S (RH-)",
+        Birthday: "March 2nd"
+    },
+    {
+        id: 6,
+        name: "Tony Tony Chopper",
+        profession: "Doctor",
+        capabilities: [
+            "Hito Hito no Mi",
+            "Drug, Medicine, and Virus Inventor",
+            "Zoolingualism",
+            "Hand-to-Hand Combatant"
+        ],
+        ethithet: "Cotton Candy Lover",
+        bounty: 1000,
+        address: "Grand Line (Drum Island)",
+        age: 17,
+        gender: "Male",
+        Height: "90cm",
+        Blood: "X",
+        Birthday: "December 24th"
+    },
+    {
+        id: 7,
+        name: "Nico Robin",
+        profession: "Archaeologist",
+        capabilities: [
+            "Hana Hana no Mi",
+            "Fish-Man Karate",
+            "Poneglyph reader",
+            "Assassination"
+        ],
+        ethithet: "Devil Child",
+        bounty:  930000000,
+        address: "West Blue",
+        age: 30,
+        gender: "Female",
+        Height: "188cm",
+        Blood: "S",
+        Birthday: "February 6th"
+    },
+    {
+        id: 8,
+        name: "Franky",
+        profession: "Shipwright",
+        capabilities: [
+            "Cyborg Modifications",
+            "Ship and Vehicle Construction",
+            {
+                BattleFrankiesArsenal: [
+                    {
+                        GeneralFranky: "Franken Swordsmanship"
+                    }
+                ]
+            },
+            "Boxing",
+            "Laser Technology"
+        ],
+        ethithet: ["Cyborg", "Iron Man"],
+        bounty:  394000000,
+        address: "South Blue",
+        age: 24,
+        gender: "Male",
+        Height: "240cm",
+        Blood: "XF",
+        Birthday: "March 9th"
+    },
+    {
+        id: 9,
+        name: "Brook",
+        profession: "Musician",
+        capabilities: [
+            "Soul Solid",
+            "Yomi Yomi no Mi",
+            "Music-based combat",
+            "Skeletal body"
+        ],
+        ethithet: ["Humming", "Soul-King"],
+        bounty: 383000000,
+        address: "West Blue",
+        age: 90,
+        gender: "Male",
+        Height: "277cm",
+        Blood: "X",
+        Birthday: "April 3rd"
+    },
+    {
+        id: 10,
+        name: "Jinbe",
+        profession: "Helmsman",
+        capabilities: [
+            "Fish-Man traits",
+            "Advance Steering Experience",
+            "Communication with Marine Life",
+            "Former Soldier",
+            {
+                FishManKarate: [
+                    "Fish-Man Jujutsu"
+                ]
+            },
+            "Fish-Man Strength",
+            "Busoshoku Haki",
+            "Kenbunshoku Haki"
+        ],
+        ethithet: "Knight of the Sea",
+        bounty:  1100000000,
+        address: "Grand Line (Ryugu Kingdom, Fish-Man Island)",
+        age: 46,
+        gender: "Male",
+        Height: "301cm",
+        Blood: "F",
+        Birthday: "April 2nd"
+    },
+];
